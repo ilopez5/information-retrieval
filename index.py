9a69ee312b072833d65d533056e425ffbe20b492
Ismael <ilopez5@hawk.iit.edu>
2019-01-31 18:40:44 -0600

    Initial surge for buildIndex

--- index.py
+++ index.py
@@ -6,16 +6,28 @@ import time
 
 class index:
 	def __init__(self,path):
+		self.path = path
 
 	def buildIndex(self):
 		#function to read documents from collection, tokenize and build the index with tokens
 		#index should also contain positional information of the terms in the document --- term: [(ID1,[pos1,pos2,..]), (ID2, [pos1,pos2,…]),….]
 		#use unique document IDs
 		index = {}
-		docID = 1		
-		for doc in collections:
-			
-			
+		docID = 1
+		punct = ".,;:'!?"		
+		for file in os.listdir(self.path):			# Walks through dir
+			tok_list = []							# Declare list for tokens
+			x = open("Text-%i.txt" %docID, r)		# Save file contents as one string 
+			tok_list = x.split().lower()			# Creates a list of lower case tokens
+			pos = 1									# Counter that marks position in document
+			for term in tok_list:					# Walks thru file tokens (already in list)
+				if term in punct:
+					continue
+				elif term not in index:
+					index[term] = [(docID, [pos])]
+					pos += 1
+				else:
+					index[term] = [(docID, [].append(pos))]
 			docID += 1
 
 
