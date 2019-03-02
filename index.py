fe29d4b7582409f23d36e8032af8dc56ab06967a
ilopez5 <ilopez5reset>
2019-03-02 12:20:58 -0600

    Pretty much finished buildIndex()

--- index.py
+++ index.py
@@ -3,13 +3,41 @@ import re
 import os
 import collections
 import time
+import math
 #import other modules as needed
 
-class index:
+class Index:
 	def __init__(self,path):
+		self.path = path
+		self.index = {}
+		self.docID_map = {}
 
 	def buildIndex(self):
 		#function to read documents from collection, tokenize and build the index with tokens
+		start = time.perf_counter()
+		for docID, doc in enumerate(os.listdir(self.path), 1):
+			with open(os.path.join(self.path, doc), 'r') as file_obj:
+				doc_string = file_obj.read().lower()
+			tok_list = filter(None, re.split(r"\W+", doc_string))
+			self.docID_map[docID] = doc
+
+			for pos, term in enumerate(tok_list, 1):
+				if term not in self.index:
+					self.index[term] = [0, (docID, 0, [pos])]
+				else:
+					for i in range(1,len(self.index[term])):
+						curr_docID, weight, positions = self.index[term][i]
+						if curr_docID == docID:
+							positions.append(pos)
+							weight = 1 + math.log10(len(positions)) # tf_td = len(positions)
+							break
+						elif i == len(self.index[term])-1:
+							self.index[term].append((docID, 1,[pos]))
+		# N = len(self.docID_map)	| df_t = len(self.index[term]) - 1	| idf_t = log10(N/df_t)
+		for term in self.index:
+			self.index[term][0] = math.log10((len(self.docID_map)) / (len(self.index[term]) - 1))
+		end = time.perf_counter()
+		print("Index built in {} seconds.".format(end-start))
 
 	def exact_query(self, query_terms, k):
 	#function for exact top K retrieval (method 1)
