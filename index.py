513b3ebc103c093440202ce3240b45e39d00f8b8
ilopez5 <ilopez5reset>
2019-03-04 00:05:59 -0600

    Working on exact query, made fcn find_match

--- index.py
+++ index.py
@@ -48,7 +48,9 @@ class Index:
 	def exact_query(self, query_terms, k):
 		#function for exact top K retrieval (method 1)
 		#Returns at the minimum the document names of the top K documents ordered in decreasing order of similarity score
-		pass
+		matches = find_match(query_terms)
+		for document in matches:
+			
 
 	def inexact_query_champion(self, query_terms, k):
 		#function for exact top K retrieval using champion list (method 2)
@@ -74,6 +76,59 @@ class Index:
 		# function to print the documents and their document id
 		for docID, doc in self.doc_list.items():
 			print("Doc ID: {0}  ==> {1}".format(docID, doc))
+
+	def find_match(self, query_terms:
+		size_q = len(query_terms)											# store size so we calculate once
+		result = []															# list to output when done
+		point = {}															# 'Pointers' to indexes for query terms
+		skip = {}															# 'Skip Pointers' 
+		base, done = True, False											# Boolean variables to be used
+		num_comp = 0														# initializing comparison counter
+		
+		for i in query_terms:
+			skip[i] = int((len(self.index[i]))**(1/2))						# Set skip pointer relative to size of word's posting list
+			point[i] = 0													# Initialize each word's pointer to 0. We will adjust these as we go
+
+		while not done:
+			for i in query_terms:											# loops through query terms
+				if base:														# base case
+					contender = self.index[i][0][0]								# set initial element to contender	
+					base = False												# So we dont do this again
+					continue
+				elif done:														# Use this to break out of for loop
+					break				
+				else:
+					while self.index[i][point[i]][0] <= contender:			# docID <= contender? This loop repeats until pointer catches up
+						if self.index[i][point[i]][0] == contender:				# docID = contender?
+							num_comp += 1											# comp ++
+							break													# break to next word
+						elif point[i] == len(self.index[i])-1:					# checks if curr_idx is last element (end condition)
+							done = True												# Set condition to stop all
+							break
+						else:
+							if point[i] <= len(self.index[i])-(skip[i]+1):			# Makes sure we dont go out of index range (for skip check)
+								if self.index[i][point[i]+skip[i]][0] < contender:	# Checks if skip pointer less than current id
+									point[i] += skip[i]								# increments pointer by the skip length
+									continue
+							point[i] += 1										# increment by 1 otherwise
+					else:													# docID > contender
+						contender = self.index[i][point[i]][0]					# set new contender 	
+						num_comp = 0											# reset comparison counter
+						continue
+					if num_comp == size_q-1 and not done:					# if contender been compared with all
+						result.append(contender)								# add contender to results list
+						if point[i] == len(self.index[i]) - 1:					# checks if curr_idx is last element
+							done = True											# set condition to stop all
+							break
+						else:												# we are safe to adjust pointers +1
+							for k in point:										# does the incrementing
+								if point[k] == len(self.index[k])-1:
+									done = True
+									break
+								point[k] += 1
+							contender = self.index[i][point[i]][0]				# sets new contender
+							num_comp = 0
+		return result
 
 if __name__ == '__main__':
 	index = Index('collection')
