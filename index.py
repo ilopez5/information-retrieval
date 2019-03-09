d3f673ee40fc6a3d0a2ffd5df15a6dda634b12f5
Ismael <44327399+ilopez5@users.noreply.github.com>
2019-03-09 10:50:43 -0600

    Exact_query and Index Compression is done

--- index.py
+++ index.py
@@ -10,167 +10,244 @@ import math
 #import other modules as needed
 
 class Index:
-	def __init__(self,path):
-		self.path = path
-		self.index = {}
-		self.docID_map = {}
+    def __init__(self,path):
+        self.path = path
+        self.index = {}
+        self.documents = {}
+        self.champion = {}
+        self.stop_list = ['a','an','and','are','as','at','be','by','for','from','has','he','in','is','it','its','of','on','that','the','to','was','were','will','with']
 
-	def buildIndex(self):
-		#function to read documents from collection, tokenize and build the index with tokens
-		start = time.perf_counter()
-		stop_list = ['a','an','and','are','as','at','be','by','for','from','has','he','in','is','it','its','of','on','that','the','to','was','were','will','with']
-		for docID, doc in enumerate(os.listdir(self.path), 1):
-			with open(os.path.join(self.path, doc), 'r') as file_obj:
-				doc_string = file_obj.read().lower()
-			tok_list = filter(None, re.split(r"\W+", doc_string))
-			self.docID_map[docID] = [doc, 0]
+    def buildIndex(self):
+        #function to read documents from collection, tokenize and build the index with tokens
+        start = time.perf_counter()
+        for docID, doc in enumerate(os.listdir(self.path), 1):
+            with open(os.path.join(self.path, doc), 'r') as file_obj:
+                doc_string = file_obj.read().lower()
+            tok_list = filter(None, re.split(r"\W+", doc_string))
+            self.documents[docID] = [doc, [], [], []]
+            for pos, term in enumerate(tok_list, 1):
+                if term in self.stop_list:
+                    continue
+                elif term not in self.index: # first time term appears in INDEX
+                    self.index[term] = [0, (docID, 1, [pos])]
+                else:
+                    for i in range(1,len(self.index[term])):
+                        curr_docID, _, positions, _ = self.index[term][i]
+                        if curr_docID == docID: # not the first time term appears in THIS document
+                            positions.append(pos)
+                            break
+                        elif i == len(self.index[term])-1: # First time term appears in this document
+                            self.index[term].append((docID, 1, [pos]))
+        ################# Index Built, Now Insert IDF & Weighted TF ##################
+        # N = len(self.documents)	| df_t = len(self.index[term]) - 1	| idf_t = log10(N/df_t)
+        for term in self.index:
+            self.index[term][0] = round(math.log10((len(self.documents)) / (len(self.index[term]) - 1)), 4) # IDF
+            for i in range(1,len(self.index[term])):
+                self.index[term][i] = (self.index[term][i][0], 1 + math.log10(len(self.index[term][i][2])), self.index[term][i][2])
+        end = time.perf_counter()
+        print("Index built in {} seconds.".format(end-start))
 
-			for pos, term in enumerate(tok_list, 1):
-				if term in stop_list:
-					continue
-				if term not in self.index:
-					self.index[term] = [0, (docID, 1, [pos])]
-					self.docID_map[docID][1] += 1
-				else:
-					for i in range(1,len(self.index[term])):
-						curr_docID, weight, positions = self.index[term][i]
-						if curr_docID == docID:
-							positions.append(pos)
-							self.docID_map[docID][1] += 1
-							weight = 1 + math.log10(len(positions)) # tf_td = len(positions)
-							break
-						elif i == len(self.index[term])-1:
-							self.index[term].append((docID, 1, [pos]))
-							self.docID_map[docID][1] += 1
-		# N = len(self.docID_map)	| df_t = len(self.index[term]) - 1	| idf_t = log10(N/df_t)
-		for term in self.index:
-			self.index[term][0] = round(math.log10((len(self.docID_map)) / (len(self.index[term]) - 1)), 4)
-		end = time.perf_counter()
-		print("Index built in {} seconds.".format(end-start))
+    def exact_query(self, query_terms, k):
+        start = time.perf_counter()
+        rank_list = [(0,0)]
+        ############### Generating Query Vector ###############################
+        q_list = re.split(r"\W+", query_terms.lower())
+        vector_q = [(term, self.index[term][0]) for term in q_list if term not in self.stop_list]
+        ltwo_q = 0
+        for i in range(len(vector_q)):
+            ltwo_q += (vector_q[i][1])**(2)
+        norm_q = math.sqrt(ltwo_q)
+        for i in range(len(vector_q)):
+            vector_q[i] = (vector_q[i][0], vector_q[i][1] / norm_q)
+        ############# Query vector is now normalized ##########################
+
+        if not len(self.documents[1][1]): # Only generates vectors once.
+            ################ Generating Doc Vectors For All Docs ##################
+            for term in self.index:
+                for i in range(1, len(self.index[term])):
+                        self.documents[self.index[term][i][0]][1].append((term, self.index[term][i][1]*self.index[term][0]))
+            #######################################################################
+            
+            ###################### Normalizing Doc Vectors ########################
+            ltwo_d = 0
+            for doc in self.documents:
+                for i in range(len(self.documents[doc][1])):
+                    ltwo_d += (self.documents[doc][1][i][1])**(2)
+                norm_d = math.sqrt(ltwo_d)
+                for i in range(len(self.documents[doc][1])):
+                    self.documents[doc][1][i] = (self.documents[doc][1][i][0], self.documents[doc][1][i][1] / norm_d)
+            #######################################################################
+
+        ################## Calculate the Cosine Score #########################
+        for doc in self.documents: # iterate over every document
+            cos_score = 0
+            for i in range(len(vector_q)): # iterate through query
+                for tup in range(len(self.documents[doc][1])): # iterate through document vector's elements
+                    if self.documents[doc][1][tup][0] == vector_q[i][0]:
+                        cos_score += self.documents[doc][1][tup][1]*vector_q[i][1]
+                        break
+                    elif tup == len(self.documents[doc][1])-1:
+                        break
+            # Inserts doc/score, keeps rank order 
+            for i in range(len(rank_list)):
+                if cos_score == 0:
+                    rank_list.append((doc, cos_score))
+                    break
+                if rank_list[i][1] <= cos_score:
+                    rank_list.insert(i, (doc, cos_score))
+                    break
+        ###################### Ranked List Now Populated ######################
+
+        ################### Print Top K & Performance Time ####################
+        for i in range(k):
+            print(f"{self.documents[rank_list[i][0]][0]:<13}, Score: {round(rank_list[i][1], 6)}")
+        end = time.perf_counter()
+        print("Results retrieved in {} seconds".format(end-start))
+        ########################## End of Function ############################
+
+    def inexact_query_champion(self, query_terms, k):
+        start = time.perf_counter()
+        rank_list = [(0,0)]
+        ############### Generating Query Vector ###############################
+        q_list = re.split(r"\W+", query_terms.lower())
+        vector_q = [(term, self.index[term][0]) for term in q_list if term not in self.stop_list]
+        ltwo_q = 0
+        for i in range(len(vector_q)):
+            ltwo_q += (vector_q[i][1])**(2)
+        norm_q = math.sqrt(ltwo_q)
+        for i in range(len(vector_q)):
+            vector_q[i] = (vector_q[i][0], vector_q[i][1] / norm_q)
+        ############# Query vector is now normalized ##########################
+
+        if not len(self.documents[1][1]): # Only generates vectors once.
+            ################ Generating Doc Vectors For All Docs ##################
+            for term in self.index:
+                for i in range(1, len(self.index[term])):
+                        self.documents[self.index[term][i][0]][1].append((term, self.index[term][i][1]*self.index[term][0]))
+            #######################################################################
+            
+            ###################### Normalizing Doc Vectors ########################
+            ltwo_d = 0
+            for doc in self.documents:
+                for i in range(len(self.documents[doc][1])):
+                    ltwo_d += (self.documents[doc][1][i][1])**(2)
+                norm_d = math.sqrt(ltwo_d)
+                for i in range(len(self.documents[doc][1])):
+                    self.documents[doc][1][i] = (self.documents[doc][1][i][0], self.documents[doc][1][i][1] / norm_d)
+            #######################################################################
+
+        ################## Calculate the Cosine Score #########################
+        for doc in self.documents: # iterate over every document
+            cos_score = 0
+            for i in range(len(vector_q)): # iterate through query
+                for tup in range(len(self.documents[doc][1])): # iterate through document vector's elements
+                    if self.documents[doc][1][tup][0] == vector_q[i][0]:
+                        cos_score += self.documents[doc][1][tup][1]*vector_q[i][1]
+                        break
+                    elif tup == len(self.documents[doc][1])-1:
+                        break
+            # Inserts doc/score, keeps rank order 
+            for i in range(len(rank_list)):
+                if cos_score == 0:
+                    rank_list.append((doc, cos_score))
+                    break
+                if rank_list[i][1] <= cos_score:
+                    rank_list.insert(i, (doc, cos_score))
+                    break
+        ###################### Ranked List Now Populated ######################
+
+        ################### Print Top K & Performance Time ####################
+        for i in range(k):
+            print(f"{self.documents[rank_list[i][0]][0]:<13}, Score: {round(rank_list[i][1], 6)}")
+        end = time.perf_counter()
+        print("Results retrieved in {} seconds".format(end-start))
+        ########################## End of Function ############################
 
 
 
-	def exact_query(self, query_terms, k):
-		#function for exact top K retrieval (method 1)
-		#Returns at the minimum the document names of the top K documents ordered in decreasing order of similarity score
-		matches = self.find_match(query_terms)
-		scores = [(0,0)]*k
-		vector_q = [self.index[term][0] for term in query_terms]
+    def inexact_query_index_elimination(self, query_terms, k):
+        #function for inexact top K retrieval using index elimination (method 3)
+        start = time.perf_counter()
+        rank_list = [(0,0)]
+        ####################### Generating Query Vector #######################
+        q_list = re.split(r"\W+", query_terms.lower())
+        vector_q = sorted([(term, self.index[term][0]) for term in q_list if term not in self.stop_list], key=lambda tup: -tup[1])
+        cut_off = len(vector_q) // 2
+        vector_q = vector_q[:-cut_off]
+        ######################## Shorter Vector Created #######################
 
-		for document in matches:
-			# Generate the document vector
-			# Find the cosine similarity score
-			# Insert it into scores list (based on score)
-			vector_d = []
-			for term in query_terms:
-				for i in range(1, len(self.index[term])):
-					if self.index[term][i][0] == document:
-						vector_d.append(self.index[term][i][1]*self.index[term][0])
-						break
+        ####################### Normalize the vector ##########################
+        ltwo_q = 0
+        for i in range(len(vector_q)):
+            ltwo_q += (vector_q[i][1])**(2)
+        norm_q = math.sqrt(ltwo_q)
+        for i in range(len(vector_q)):
+            vector_q[i] = (vector_q[i][0], vector_q[i][1] / norm_q)
+        ############# Query vector is now normalized ##########################
 
-			score = 0
-			for j in range(len(vector_q)):	
-				score += vector_q[j]*vector_d[j]
+        if not len(self.documents[2][2]): # Only generates vectors once.
+            ################ Generating Doc Vectors For All Docs ##################
+            for term in self.index:
+                for i in range(1, len(self.index[term])):
+                    self.documents[self.index[term][i][0]][2].append((term, self.index[term][i][1]*self.index[term][0]))
+            #######################################################################
+            
+            ###################### Normalizing Doc Vectors ########################
+            ltwo_d = 0
+            for doc in self.documents:
+                for i in range(len(self.documents[doc][2])):
+                    ltwo_d += (self.documents[doc][2][i][1])**(2)
+                norm_d = math.sqrt(ltwo_d)
+                for i in range(len(self.documents[doc][2])):
+                    self.documents[doc][2][i] = (self.documents[doc][2][i][0], self.documents[doc][2][i][1] / norm_d)
+            #######################################################################
 
-			norm = (len(vector_q)*len(vector_d))**(2)
-			score = score / norm
+        ################## Calculate the Cosine Score #########################
+        for doc in self.documents: # iterate over every document
+            cos_score = 0
+            for i in range(len(vector_q)): # iterate through query
+                for term in range(len(self.documents[doc][2])): # iterate through document vector's elements
+                    if self.documents[doc][2][term][0] == vector_q[i][0]:
+                        cos_score += self.documents[doc][2][term][1]*vector_q[i][1]
+                        break
+                    elif term == len(self.documents[doc][2])-1:
+                        break
+            # Inserts doc/score, keeps rank order
+            for i in range(len(rank_list)):
+                if cos_score == 0:
+                    rank_list.append((doc, cos_score))
+                    break
+                if rank_list[i][1] <= cos_score:
+                    rank_list.insert(i, (doc, cos_score))
+                    break
+        ###################### Ranked List Now Populated ######################
 
-			for y in range(len(vector_q)):
-				if scores[y][1] <= score:
-					scores.insert(y, (document, score))
-					break
+        ################### Print Top K & Performance Time ####################
+        for i in range(k):
+            print(f"{self.documents[rank_list[i][0]][0]:<13}, Score: {round(rank_list[i][1], 6)}")
+        end = time.perf_counter()
+        print("Results retrieved in {} seconds".format(end-start))
+        ########################## End of Function ############################
 
-		# Once complete
+    def inexact_query_cluster_pruning(self, query_terms, k):
+        #function for exact top K retrieval using cluster pruning (method 4)
+        #Returns at the minimum the document names of the top K docIDs ordered in decreasing order of similarity score
+        pass
 
-		for x in range(k):
-			if scores[x] == (0,0):
-				break
-			print(self.docID_map[scores[x][0]][0], " Score: {0}".format(round(scores[x][1], 4)))
+    def print_dict(self):
+        #function to print the terms and posting list in the index
+        for term,pos_list in self.index.items():
+            print(term, ':', pos_list)
 
-
-
-
-
-
-	def inexact_query_champion(self, query_terms, k):
-		#function for exact top K retrieval using champion list (method 2)
-		#Returns at the minimum the document names of the top K documents ordered in decreasing order of similarity score
-		pass
-
-	def inexact_query_index_elimination(self, query_terms, k):
-		#function for exact top K retrieval using index elimination (method 3)
-		#Returns at the minimum the document names of the top K documents ordered in decreasing order of similarity score
-		pass
-
-	def inexact_query_cluster_pruning(self, query_terms, k):
-		#function for exact top K retrieval using cluster pruning (method 4)
-		#Returns at the minimum the document names of the top K documents ordered in decreasing order of similarity score
-		pass
-
-	def print_dict(self):
-		#function to print the terms and posting list in the index
-		for term,pos_list in self.index.items():
-			print(term, ':', pos_list)
-
-	def print_doc_list(self):
-		# function to print the documents and their document id
-		for docID, doc in self.doc_list.items():
-			print("Doc ID: {0}  ==> {1}".format(docID, doc))
-
-	def find_match(self, query_terms):
-		size_q = len(query_terms)											# store size so we calculate once
-		result = []															# list to output when done
-		point = {}															# 'Pointers' to indexes for query terms
-		skip = {}															# 'Skip Pointers' 
-		base, done = True, False											# Boolean variables to be used
-		num_comp = 0														# initializing comparison counter
-		
-		for i in query_terms:
-			skip[i] = int((len(self.index[i]))**(1/2))						# Set skip pointer relative to size of word's posting list
-			point[i] = 1													# Initialize each word's pointer to 0. We will adjust these as we go
-
-		while not done:
-			for i in query_terms:											# loops through query terms
-				if base:														# base case
-					contender = self.index[i][1][0]								# set initial element to contender	
-					base = False												# So we dont do this again
-					continue
-				elif done:														# Use this to break out of for loop
-					break				
-				else:
-					while self.index[i][point[i]][0] <= contender:			# docID <= contender? This loop repeats until pointer catches up
-						if self.index[i][point[i]][0] == contender:				# docID = contender?
-							num_comp += 1											# comp ++
-							break													# break to next word
-						elif point[i] == len(self.index[i])-1:					# checks if curr_idx is last element (end condition)
-							done = True												# Set condition to stop all
-							break
-						else:
-							if point[i] <= len(self.index[i])-(skip[i]+1):			# Makes sure we dont go out of index range (for skip check)
-								if self.index[i][point[i]+skip[i]][0] < contender:	# Checks if skip pointer less than current id
-									point[i] += skip[i]								# increments pointer by the skip length
-									continue
-							point[i] += 1										# increment by 1 otherwise
-					else:													# docID > contender
-						contender = self.index[i][point[i]][0]					# set new contender 	
-						num_comp = 0											# reset comparison counter
-						continue
-					if num_comp == size_q-1 and not done:					# if contender been compared with all
-						result.append(contender)								# add contender to results list
-						if point[i] == len(self.index[i]) - 1:					# checks if curr_idx is last element
-							done = True											# set condition to stop all
-							break
-						else:												# we are safe to adjust pointers +1
-							for k in point:										# does the incrementing
-								if point[k] == len(self.index[k])-1:
-									done = True
-									break
-								point[k] += 1
-							contender = self.index[i][point[i]][0]				# sets new contender
-							num_comp = 0
-		return result
+    def print_doc_list(self):
+        # function to print the documents and their document id
+        for docID, doc in self.documents.items():
+            print("Doc ID: {0}  ==> {1}".format(docID, doc))
 
 if __name__ == '__main__':
-	index = Index('collection')
-	index.buildIndex()
-	index.exact_query(['red', 'china'], 10)
+    index = Index('collection')
+    index.buildIndex()
+    index.exact_query('red china home', 5)
+    index.inexact_query_index_elimination('red china home', 5)
+    #index.inexact_query_champion('red china home', 5)
+    #index.inexact_query_cluster_pruning('red china home', 5)
