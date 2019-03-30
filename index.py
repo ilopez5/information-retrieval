f14a1f42581b5cb6c5a16cad399f503ecb8de311
Ismael <ilopez5@hawk.iit.edu>
2019-03-30 13:44:02 -0500

    Nearly done with Rocchio, should have modified query done now

--- index.py
+++ index.py
@@ -12,12 +12,13 @@ class Index:
 		self.index = {}							 # term: [idf, (docID, wtf, [positions]), ...)
 		self.docmap = {}						 # docID: [doc#, len(doc)]
 		with open(stop_name) as f:
-			self.stop = f.read().lower().split() # ['a', 'about', 'above', ...]		
+			self.stop = f.read().lower().split() # ['a', 'about', 'above', ...]
+		self.rocchio = 0
 		
 	def buildIndex(self):
-		# function to read documents from collection, tokenize and build the index with tokens
-		# implement additional functionality to support relevance feedback
-		# use unique document integer IDs
+		# function to read documents from collection, tokenize and build the 
+		# index with tokens implement additional functionality to support 
+		# relevance feedback use unique document integer IDs
 		start = time.perf_counter()
 		file = ""
 		with open(self.path, 'r') as f:						# Opens file
@@ -64,28 +65,45 @@ class Index:
 			dft = len(self.index[term]) - 1					# Length of Post List
 			self.index[term][0] = round(m.log10(N/dft), 8)	# Insert rounded IDF
 		end = time.perf_counter()
-		print("Index built in {} seconds.".format(end-start))
+		print("TF-IDF Index built in {} seconds.".format(end-start))
+		return
 
 	
-	def rocchio(self, query_terms, pos_feedback, neg_feedback, alpha, beta, gamma):
-	# function to implement rocchio algorithm
-	# pos_feedback - documents deemed to be relevant by the user
-	# neg_feedback - documents deemed to be non-relevant by the user
-	# Return the new query  terms and their weights
-		pass
+	def rocchio(self, query_terms):
+		# function to implement rocchio algorithm
+		# Return the new query  terms and their weights
+		start = time.perf_counter()
+		beta, gamma = .75, .15								# Parameters given
+		Done = False
+		self.rocchio += 1									# Tracks iteration
+		query = self.calc_query(query_terms)
+		while not Done:
+			print("=== Rocchio Algorithm ===\nIteration:",self.counter)
+			pos_fb = input("Enter relevant document ids separated by space:").split()
+			neg_fb = input("Enter non-relevant document ids separated by soace:").split()
+			pos_fb, neg_fb = self.string_to_int(pos_fb, neg_fb)
+			rel_doc, nrel_doc = self.create_doc_vectors(pos_fb, neg_fb, beta, gamma)
+			new_query = self.add_vectors(query, self.add_vectors(rel_doc, nrel_doc))
+			# we have new query now
+			
 
 	def query(self, query_terms, k):
 		# function for exact top K retrieval using cosine similarity
-		# Returns at the minimum the document names of the top K documents ordered in decreasing order of similarity score
+		# Returns at the minimum the document names of the top K documents ordered 
+		# in decreasing order of similarity score
 		start = time.perf_counter()
+		print("Query to search:", query_terms)
+		print("Number of (top) results: {}\n".format(k))
 		query, query_length = self.calc_tfidf_query(query_terms) # Calc tfidf for query
 		results = self.cosine_score(query, query_length)		 # Calc cosine score
 		end = time.perf_counter()
+		print("Top {0} result(s) for the query {1} are:\n".format(k, query_terms))
+		print(f"Doc id:  Doc Name:     Score:")
 		for count, (doc_id, score) in enumerate(results, 0):
 			if count == k:
 				break
-			print("{0}. {1} with score {2}".format(count+1,self.docmap[doc_id][0],score))	
-		print("Results found in {} seconds.".format(end-start))
+			print(f"{doc_id:<8} TEXT {self.docmap[doc_id][0]}.txt, {score}")	
+		print("\nResults found in {} seconds.\n".format(end-start))
 
 	def print_dict(self):
 		# function to print the terms and posting list in the index
@@ -95,17 +113,17 @@ class Index:
 	def print_doc_list(self):
 		# function to print the documents and their document id
 		for docID, doc in self.docmap.items():
-			print("Doc ID: {0}		==> {1}".format(docID, doc[0]))
+			print("Doc ID: {0}	==> {1}".format(docID, doc[0]))
 
 	def cosine_score(self, query, query_length):
-		# calculate the cosine similarity score of any two vectors passed in
+		# Calculate the cosine similarity score of any two vectors passed in
 		scores = c.defaultdict(int)										# Store rank list
 		doc_tfidf = c.defaultdict(float)								# Store doc tfidf
 
 		# Calculate numerator per doc
 		for query_term, query_tfidf in query.items():
 			doc_idf = self.index[query_term][0]							# Store IDF
-			for index, item in enumerate(self.index[query_term][1:]):
+			for item in self.index[query_term][1:]:
 				doc_id = item[0]										# docID
 				doc_wtf = item[1]										# wtf
 				doc_tfidf[doc_id] = doc_idf*doc_wtf						# Calculate tfidf
@@ -118,6 +136,35 @@ class Index:
 				continue
 			scores[doc_id] /= denom										# Normalize
 		return sorted(scores.items(), key=lambda x: x[1], reverse=True) # Return sorted list
+
+	def create_doc_vector(self, r_docs, nr_docs, beta, gamma):
+		b_dr = beta / len(pos_fb)
+		g_dnr = gamma / len(neg_fb)
+		rel_vect = c.defaultdict(float)				# Relevant doc vector
+		nrel_vect = c.defaultdict(float)			# Non relevant doc vector
+		
+		nrsum, rsum = 0, 0
+		for term in self.index:						# Iterate -> all terms
+			for item in self.index[term][1:]:		# Iterate -> post list
+				pos, docID = item[2], item[0]
+				if docID in r_docs:					# Curr doc in D_r
+					rsum += len(pos)
+				elif docID in nr_docs:				# Curr doc in D_nr
+					nrsum += len(pos)
+			if rsum:								# Term => new query
+				rel_vect[term] = rsum				# Add raw freq
+				if nrsum:
+					nrel_vect[term] = nrsum
+			elif nrsum:
+				nrel_vect[term] = nrsum
+		# Scaling with Beta and Gamma values
+		for term, freq in rel_vect.items():
+			rel_vect[term] = freq * b_dr
+		for term, freq in nrel_vect.items():
+			nrel_vect[term] = freq * g_dnr
+
+		return rel_vect, nrel_vect
+
 
 	def calc_tfidf_query(self, query_terms):
 		# Converts query into vector (taken from HW 2 Solution)
@@ -148,7 +195,48 @@ class Index:
 		query_length = m.sqrt(query_length)
 		return query, query_length
 
+	def calc_query(self, query_terms):
+		# Calculates a simple query vector (e.g. [1,1,2]
+		query = c.defaultdict(int)
+		tokens = re.split('\W+', query_terms.lower())
+		for term in tokens:
+			if term == '':
+				continue
+			elif term in self.stop:
+				continue
+			elif term not in self.index.keys():
+				continue
+			elif term not in query:
+				query[term] = 1
+		return query
+
+	def add_vectors(self, v_one, v_two):
+		# This method will be used to sum vectors
+		new_query = c.defaultdict(float)
+		
+		for term,val in v_one.items():
+			if term not in v_two:
+				new_query[term] = val
+			else:
+				new_query[term] = val + v_two[term]
+		for term,val in v_two.items():
+			if term in new_query:
+				continue
+			else:
+				new_query[term] = val
+		return new_query
+
+	def string_to_int(self, pos, neg):
+		# Converts user feedback into integers
+		for i in range(len(pos)):
+			pos[i] = eval(pos[i])
+		for j in range(len(neg)):
+			neg[j] = eval(neg[j])
+		return pos, neg
+
+
 if __name__ == '__main__':
 	x = Index('TIME.ALL', 'TIME.STP')
 	x.buildIndex()
-	x.query('red china home', 10)	
+	x.query('red china home', 10)
+#	x.rocchio('red china home')	
