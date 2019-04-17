a36cad56fb342571dd3127c5543e93c5a4bf82e7
Ismael <ilopez5@hawk.iit.edu>
2019-04-16 20:03:52 -0500

    Adding files from bonus assignment. Initial push.

---
+++ Bonus/kmeans.py
@@ -0,0 +1,18 @@
+#Python 3.0
+import re
+import os
+import collections
+import time
+#import other modules as needed
+#implement other functions as needed
+class kmeans:
+	def __init__(self,path_to_collection):
+
+	def clustering(self, kvalue):
+	#function to implement kmeans clustering algorithm
+	#Print out:
+	##For each cluster, its RSS values and the document ID of the document closest to its centroid.
+    ##Average RSS value
+	##Time taken for computation.
+
+	
